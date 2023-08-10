a1db312f019abb7b1f408463ba0fd611b89daa59
Kalen White <kalen@krworks.com>
2023-08-09 19:26:31 -0700

    update many build args

--- overlays/skia/build.sh
+++ overlays/skia/build.sh
@@ -9,7 +9,9 @@ python3 tools/git-sync-deps
 # trivial abi is what gets me.. who wouldnt want a trivial abi.. who rationalizes having an option for that
 # everything is good but your abi looks too trivial for us
 gn gen out/Vulkan --args='
-is_official_build=false is_debug=false is_component_build=false is_trivial_abi=true werror=true 
+is_official_build=false skia_enable_tools=false skia_use_gl=false skia_use_expat=false 
+skia_enable_fontmgr_empty=true skia_enable_svg=false skia_use_icu=false is_debug=true 
+is_component_build=false is_trivial_abi=true werror=true skia_use_fonthost_mac=false 
 skia_use_angle=false skia_use_dng_sdk=false skia_use_dawn=false skia_use_webgl=false 
 skia_use_webgpu=false skia_use_expat=false skia_use_fontconfig=false skia_use_freetype=true 
 skia_use_libheif=false skia_use_libjpeg_turbo_decode=false skia_use_libjpeg_turbo_encode=false 
@@ -17,7 +19,7 @@ skia_use_no_jpeg_encode=false skia_use_libpng_decode=false skia_use_libpng_encod
 skia_use_no_png_encode=false skia_use_libwebp_decode=false skia_use_libwebp_encode=false 
 skia_use_no_webp_encode=false  skia_use_lua=false skia_use_piex=false skia_use_system_freetype2=false 
 skia_use_system_libpng=false skia_use_system_zlib=false skia_use_vulkan=true skia_use_wuffs=false 
-skia_use_zlib=false skia_enable_ganesh=false skia_enable_sksl=false skia_build_for_debugger=false 
+skia_use_zlib=false skia_enable_ganesh=true skia_enable_sksl=true skia_build_for_debugger=false 
 skia_enable_sksl_tracing=false skia_enable_skshaper=false skia_enable_skparagraph=false skia_enable_pdf=false 
 skia_canvaskit_force_tracing=false skia_canvaskit_profile_build=false skia_canvaskit_enable_skp_serialization=false 
 skia_canvaskit_enable_effects_deserialization=false skia_canvaskit_enable_skottie=false 
