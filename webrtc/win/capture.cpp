989d11010a32975738e952e942dbfa0f23fc4279
Kalen White <kalen@krworks.com>
2023-09-15 13:12:22 -0700

    get implementation streaming on windows; must test multi thread apt mode

---
+++ webrtc/win/capture.cpp
@@ -0,0 +1,291 @@
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <d3d11.h>
+#include <Inspectable.h>
+#include <winrt/base.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.Capture.h>
+#include <windows.graphics.capture.h>
+#include <windows.graphics.capture.interop.h>
+#include <windows.ui.xaml.media.dxinterop.h>
+#include <windows.graphics.directx.direct3d11.interop.h>
+
+#include <nvenc/nvEncodeAPI.h>
+#include <webrtc/nvenc/NvCodecUtils.h>
+#include <webrtc/nvenc/NvEncoderD3D11.h>
+#include <webrtc/nvenc/NvEncoderCLIOptions.h>
+
+using GraphicsCaptureItem           = winrt::Windows::Graphics::Capture::GraphicsCaptureItem;
+using GraphicsCaptureSession        = winrt::Windows::Graphics::Capture::GraphicsCaptureSession;
+using IDirect3DDxgiInterfaceAccess  = Windows::Graphics::DirectX::Direct3D11::IDirect3DDxgiInterfaceAccess;
+using DirectXPixelFormat            = winrt::Windows::Graphics::DirectX::DirectXPixelFormat;
+using IDirect3DDevice               = winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice;
+template <typename T>
+using com_ptr                       = winrt::com_ptr<T>;
+using Direct3D11CaptureFrame        = winrt::Windows::Graphics::Capture::Direct3D11CaptureFrame;
+using Direct3D11CaptureFramePool    = winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool;
+using SizeInt32                     = winrt::Windows::Graphics::SizeInt32;
+using IGraphicsCaptureItem          = ABI::Windows::Graphics::Capture::IGraphicsCaptureItem;
+using DispatcherQueueController     = winrt::Windows::System::DispatcherQueueController;
+
+using namespace std;
+using namespace ion;
+
+#define errorf(...) do { fprintf(stderr, __VA_ARGS__); exit(1); } while (0);
+
+extern "C"
+{
+    HRESULT __stdcall CreateDirect3D11DeviceFromDXGIDevice(::IDXGIDevice* dxgiDevice,
+        ::IInspectable** graphicsDevice);
+
+    HRESULT __stdcall CreateDirect3D11SurfaceFromDXGISurface(::IDXGISurface* dgxiSurface,
+        ::IInspectable** graphicsSurface);
+}
+
+auto CreateDXGISwapChain(winrt::com_ptr<ID3D11Device> const& device, const DXGI_SWAP_CHAIN_DESC1* desc)
+{
+    auto dxgiDevice = device.as<IDXGIDevice2>();
+    winrt::com_ptr<IDXGIAdapter> adapter;
+    winrt::check_hresult(dxgiDevice->GetParent(winrt::guid_of<IDXGIAdapter>(), adapter.put_void()));
+    winrt::com_ptr<IDXGIFactory2> factory;
+    winrt::check_hresult(adapter->GetParent(winrt::guid_of<IDXGIFactory2>(), factory.put_void()));
+
+    winrt::com_ptr<IDXGISwapChain1> swapchain;
+    winrt::check_hresult(factory->CreateSwapChainForComposition(device.get(), desc, nullptr, swapchain.put()));
+    return swapchain;
+}
+
+auto CreateDXGISwapChain(com_ptr<ID3D11Device> const& device,
+    uint32_t width, uint32_t height, DXGI_FORMAT format, uint32_t bufferCount)
+{
+    DXGI_SWAP_CHAIN_DESC1 desc = {};
+    desc.Width = width;
+    desc.Height = height;
+    desc.Format = format;
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.SampleDesc.Count = 1;
+    desc.SampleDesc.Quality = 0;
+    desc.BufferCount = bufferCount;
+    desc.Scaling = DXGI_SCALING_STRETCH;
+    desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+    desc.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
+
+    return CreateDXGISwapChain(device, &desc);
+}
+
+template <typename T>
+auto GetDXGIInterfaceFromObject(winrt::Windows::Foundation::IInspectable const& object)
+{
+    auto access = object.as<IDirect3DDxgiInterfaceAccess>();
+    winrt::com_ptr<T> result;
+    winrt::check_hresult(access->GetInterface(winrt::guid_of<T>(), result.put_void()));
+    return result;
+}
+
+struct iCapture {
+    protected:
+    struct NVenc {
+        void* enc;
+        NV_ENC_REGISTER_RESOURCE       res       { NV_ENC_REGISTER_RESOURCE_VER };
+        NV_ENC_MAP_INPUT_RESOURCE      input     { NV_ENC_MAP_INPUT_RESOURCE_VER };
+        NV_ENCODE_API_FUNCTION_LIST    fn        { NV_ENCODE_API_FUNCTION_LIST_VER };
+        NV_ENC_CREATE_BITSTREAM_BUFFER bitstream { NV_ENC_CREATE_BITSTREAM_BUFFER_VER };
+    } nv;
+
+    public:
+
+    inline static DispatcherQueueController dispatcher { nullptr };
+    inline static com_ptr<  ID3D11Device>   d3d_device;
+    inline static com_ptr<::IInspectable>   d3d_device_i;
+
+    bool async_start = false;
+    std::mutex mtx;
+    
+    GraphicsCaptureItem                 m_item          { nullptr };
+    IDirect3DDevice                     m_device        { nullptr };
+    com_ptr<ID3D11DeviceContext>        m_d3dContext    { nullptr };
+    Direct3D11CaptureFramePool          m_framePool     { nullptr };
+    GraphicsCaptureSession              m_session       { nullptr };
+    SizeInt32                           m_lastSize;
+    com_ptr<IDXGISwapChain1>            m_swapChain     { nullptr };
+    DirectXPixelFormat                  m_pixelFormat;
+    uint32_t                            width = 0, height = 0;
+    lambda<bool(mx)>                    m_output;
+    atomic<optional<DirectXPixelFormat>> m_pixelFormatUpdate = std::nullopt;
+    atomic<bool>                        m_closed           = false;
+    atomic<bool>                        m_captureNextImage = false;
+    ID3D11DeviceContext*                d3d11_context;
+    com_ptr<ID3D11Texture2D>            m_TexSysMem;
+    NvEncoderD3D11 *                    nvenc;
+
+    void OnFrameArrived(Direct3D11CaptureFramePool const& sender, winrt::Windows::Foundation::IInspectable const&) {
+        Direct3D11CaptureFrame frame = sender.TryGetNextFrame();
+        auto surfaceTexture = GetDXGIInterfaceFromObject<ID3D11Texture2D>(frame.Surface());
+        mx            bytes = Encode(surfaceTexture.get());
+        SizeInt32        sz = frame.ContentSize();
+        if (bytes)
+            if (!m_output(bytes)) {
+                Close();
+            }
+        if (sz != m_lastSize) {
+            m_lastSize = sz; 
+            m_framePool.Recreate(m_device, m_pixelFormat, 2, m_lastSize);
+        }
+    }
+
+    void init(HWND hwnd, lambda<bool(mx)> on_encoded) {
+        static bool init = false;
+        static int sessions = 0;
+        static std::mutex mtx;
+
+        /// lets protect during init
+        mtx.lock();
+        if (!init) {
+            init = true;
+            winrt::init_apartment(winrt::apartment_type::single_threaded);
+            dispatcher = DispatcherQueueController::CreateOnDedicatedThread();
+
+            if (!GraphicsCaptureSession::IsSupported())
+                errorf("Screen capture is not supported on this device for this release of Windows!\n");
+            
+            winrt::check_hresult(
+                D3D11CreateDevice(
+                    nullptr, D3D_DRIVER_TYPE_HARDWARE,   nullptr,
+                    D3D11_CREATE_DEVICE_BGRA_SUPPORT,    nullptr, 0,
+                    D3D11_SDK_VERSION, d3d_device.put(), nullptr, nullptr));
+            
+            auto dxgi_device = d3d_device.as<IDXGIDevice>();
+            winrt::check_hresult(
+                CreateDirect3D11DeviceFromDXGIDevice(
+                    dxgi_device.get(), d3d_device_i.put()));
+        }
+        sessions++;
+        mtx.unlock();
+
+        /// get context info
+        ID3D11Device *d3d11 = d3d_device.get();
+        m_device = d3d_device_i.as<IDirect3DDevice>();
+        auto d3dDevice     = GetDXGIInterfaceFromObject<ID3D11Device>(m_device);
+        d3dDevice->GetImmediateContext(m_d3dContext.put());
+        d3d11_context      = m_d3dContext.get();
+
+        /// get window item
+        auto interop_factory = winrt::get_activation_factory<GraphicsCaptureItem, IGraphicsCaptureItemInterop>();
+        winrt::check_hresult(
+            interop_factory->CreateForWindow(
+                hwnd, winrt::guid_of<IGraphicsCaptureItem>(), winrt::put_abi(m_item)));
+
+        /// instance nvenc
+        assert(NV_ENC_SUCCESS == NvEncodeAPICreateInstance(&nv.fn));
+        NV_ENC_OPEN_ENCODE_SESSION_EX_PARAMS startup { NV_ENC_OPEN_ENCODE_SESSION_EX_PARAMS_VER };
+        startup.device     = (void*)d3d11;
+        startup.deviceType = NV_ENC_DEVICE_TYPE_DIRECTX;
+        startup.apiVersion = NVENCAPI_VERSION;
+
+        /// open encode session in nvenc
+        assert(NV_ENC_SUCCESS == nv.fn.nvEncOpenEncodeSessionEx(&startup, &nv.enc));
+
+        /// set data members
+        m_pixelFormat      = DirectXPixelFormat::B8G8R8A8UIntNormalized;
+        m_lastSize         = m_item.Size();
+        width              = m_lastSize.Width;
+        height             = m_lastSize.Height;
+        m_output           = on_encoded;
+        m_framePool        = Direct3D11CaptureFramePool::Create(m_device, m_pixelFormat, 2, m_item.Size());
+        m_session          = m_framePool.CreateCaptureSession(m_item);
+        
+        /// set frame processor
+        m_framePool.FrameArrived({ this, &Capture::OnFrameArrived });
+
+        /// allocate nvenc and set initial config
+        nvenc = new NvEncoderD3D11(
+            d3d11, width, height, NV_ENC_BUFFER_FORMAT_ARGB, 0, false, false);
+        
+        /// supplement configuration
+        NV_ENC_INITIALIZE_PARAMS initializeParams { NV_ENC_INITIALIZE_PARAMS_VER };
+        NV_ENC_CONFIG            encodeConfig     { NV_ENC_CONFIG_VER };
+        initializeParams.encodeConfig = &encodeConfig;
+        nvenc->CreateDefaultEncoderParams(
+            &initializeParams,
+             NV_ENC_CODEC_H264_GUID,
+             NV_ENC_PRESET_P3_GUID,
+             NV_ENC_TUNING_INFO_HIGH_QUALITY);
+
+        NvEncoderInitParam cli;
+        cli.SetInitParams(&initializeParams, NV_ENC_BUFFER_FORMAT_ARGB);
+
+        /// create nvencoder
+        nvenc->CreateEncoder(&initializeParams);
+
+        /// start capturing window item
+        m_session.StartCapture();
+    }
+
+    /// control for how many frames it encodes
+    mx Encode(ID3D11Texture2D *texture)
+    {
+        std::vector<std::vector<uint8_t>> vPacket;
+        const NvEncInputFrame* input_frame = nvenc->GetNextInputFrame();
+        ID3D11Texture2D *pTexInput = reinterpret_cast<ID3D11Texture2D*>(input_frame->inputPtr);
+        d3d11_context->CopyResource(pTexInput, texture);
+        nvenc->EncodeFrame(vPacket);
+        return vPacket.size() ? mx { (u8*)vPacket[0].data(), vPacket[0].size() } : mx {};
+    }
+
+    void Close()
+    {
+        nvenc->DestroyEncoder();
+        delete nvenc;
+        m_session.Close();
+        m_framePool.Close();
+        m_framePool = nullptr;
+        m_session   = nullptr;
+        m_item      = nullptr;
+    }
+};
+
+mx_implement(Capture, mx);
+
+/// async makes case for context-data schema strong
+Capture::Capture(HWND hwnd, lambda<bool(mx)> encoded):Capture() {
+    async { 1, [data](runtime *rt, int idx) {
+        data->mtx.lock();
+        data->async_start = true;
+        data->init(hwnd, encoded);
+        data->mtx.unlock();
+
+        MSG     msg { 0 };
+        while (GetMessage(&msg, nullptr, 0, 0)) {
+            if (!data->nvenc)
+                break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+    }};
+}
+
+void Capture::stop() {
+    while (!data->async_start) {
+        usleep(100);
+    }
+    data->Close();
+}
+
+
+
+int test_for_window(char *win) {    
+    HWND hwnd = FindWindowA(NULL, win);
+
+    lambda<bool(mx)> encoded = [](mx data) -> bool {
+        printf("data encoded: %d bytes\n", data.count());
+        return true;
+    };
+
+    Capture capture { hwnd, encoded };
+
+
+    int test = 0;
+    test++;
+}
