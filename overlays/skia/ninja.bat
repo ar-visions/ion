5e2280a9c7a2e416dd4f9f81d1928cc3bff70bab
Kalen White <kalen@krworks.com>
2023-09-24 13:20:05 -0700

    fix arg

--- overlays/skia/ninja.bat
+++ overlays/skia/ninja.bat
@@ -1,9 +1,11 @@
 @echo on
 
 setlocal enabledelayedexpansion
-set sdk=!%~1!
+set sdk=%1
 
 third_party\ninja\ninja -C out\Vulkan
+
+echo Sdk is set to %sdk%
 
 xcopy include ..\..\install\%sdk%\include\skia /E /I /Y
 copy /Y out\Vulkan\skia.lib ..\..\install\%sdk%\lib\
