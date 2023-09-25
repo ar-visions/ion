e2b0acb235e064028bbe07e10ed1fd23804cc639
Kalen White <kalen@krworks.com>
2023-09-25 14:11:02 -0700

    debug introspection in the ux

--- ux/ux.hpp
+++ ux/ux.hpp
@@ -512,7 +512,7 @@ struct Element:node {
         bool                    capture;
         bool                    hover;
         bool                    active;
-        bool                    focus;
+        bool                    focus = true;
         int                     tab_index;
         vec2d                   cursor;
 
@@ -631,7 +631,6 @@ struct Element:node {
                 prop { "text-align",     drawings[operation::text]   .align   },
 
                 prop { "capture",   capture   },
-                prop { "focuse",    focus     },
                 prop { "hover",     hover     },
                 prop { "active",    active    },
                 prop { "focus",     focus     },
@@ -1078,7 +1077,7 @@ struct App:composer {
                 rectd &bounds = ee->data->fill_bounds ? ee->data->fill_bounds : ee->data->bounds;
                 bool in = bounds.contains(rel);
                 e->data->cursor = in ? vec2d(x, y) : vec2d(-1, -1);
-                return (in && (!active || !edata->active)) ? e : null;
+                return (in && (!active || !edata->active)) ? ee : null;
             });
             array<Element*> actives = e->select([&](Element *ee) -> Element* {
                 return (active && ee->data->active) ? ee : null;
