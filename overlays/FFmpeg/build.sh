291f71b257713abe1b048f167e9b5ab274b4dfb5
Kalen Novis White <kalen@krworks.com>
2023-11-28 13:07:33 -0700

    CMake-compatible build for Linux/Mac (Windows required now) -- Also needs x264 built prior

---
+++ overlays/FFmpeg/build.sh
@@ -0,0 +1,26 @@
+./configure \
+	--pkgconfigdir=$1/lib/pkgconfig \
+	--pkg-config-flags="--static" \
+	--arch=x86_64 \
+	--disable-doc \
+	--disable-everything \
+	--enable-gpl \
+	--enable-libx264 \
+	--enable-encoder=libx264 \
+	--enable-decoder=h264 \
+	--enable-muxer=mp4 \
+	--enable-muxer=matroska \
+	--enable-demuxer=mov \
+	--enable-demuxer=matroska \
+	--enable-parser=h264 \
+	--enable-protocol=file \
+	--prefix="$1" \
+	--enable-debug \
+	--enable-libopus \
+	--enable-libfdk-aac \
+	--enable-encoder=libopus \
+	--enable-encoder=libfdk_aac \
+	--enable-decoder=opus \
+	--enable-decoder=aac \
+	--enable-parser=opus \
+	--enable-nonfree && make -j16
