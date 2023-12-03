89c3ed10c97185f273cbb818d853ea01537721ea
Kalen Novis White <kalen@krworks.com>
2023-12-02 21:31:52 -0700

    fix skia up from external debug/release feature added to prefix

--- overlays/skia/build.sh
+++ overlays/skia/build.sh
@@ -6,8 +6,11 @@ PYTHON3="python3"
 GN="gn"
 NINJA="ninja"
 sdk=$1
+cfg=$2
 cxx="clang++"
 cc="clang"
+dbg="true"
+
 
 if [ -n "$MSYSTEM" ]; then
     PYTHON3="/mingw64/bin/python3"
@@ -20,10 +23,14 @@ if [ $sdk != "native" ]; then
     cc="$sdk-gcc"
 fi
 
+if [ $cfg = "release" ]; then
+    dbg="false"
+fi
+
 # run the command to sync dependencies
 $PYTHON3 tools/git-sync-deps
 
-$GN gen out/Vulkan --args='is_official_build=false cxx="clang++" cc="clang" skia_use_vulkan=true skia_use_vma=true skia_enable_gpu=true skia_enable_tools=false skia_use_gl=false skia_use_expat=true skia_enable_fontmgr_empty=false skia_enable_svg=true skia_use_icu=true is_debug=false is_component_build=false is_trivial_abi=false werror=true skia_use_fonthost_mac=false skia_use_angle=false skia_use_dng_sdk=false skia_use_dawn=false skia_use_webgl=false skia_use_webgpu=false skia_use_fontconfig=false skia_use_freetype=true skia_use_libheif=false skia_use_libjpeg_turbo_decode=false skia_use_libjpeg_turbo_encode=false skia_use_no_jpeg_encode=false skia_use_libpng_decode=true skia_use_libpng_encode=true skia_use_no_png_encode=false skia_use_libwebp_decode=false skia_use_libwebp_encode=false skia_use_no_webp_encode=false  skia_use_lua=false skia_use_piex=false skia_use_system_freetype2=false skia_use_system_libpng=false skia_use_system_zlib=false skia_use_wuffs=false skia_use_zlib=false skia_enable_ganesh=true skia_build_for_debugger=false skia_enable_sksl_tracing=false skia_enable_skshaper=false skia_enable_skparagraph=false skia_enable_pdf=false skia_canvaskit_force_tracing=false skia_canvaskit_profile_build=false skia_canvaskit_enable_skp_serialization=false skia_canvaskit_enable_effects_deserialization=false skia_canvaskit_enable_skottie=false skia_canvaskit_include_viewer=false skia_canvaskit_enable_pathops=false skia_canvaskit_enable_rt_shader=false skia_canvaskit_enable_matrix_helper=false skia_canvaskit_enable_canvas_bindings=false skia_canvaskit_enable_font=false skia_canvaskit_enable_embedded_font=false skia_canvaskit_enable_alias_font=false skia_canvaskit_legacy_draw_vertices_blend_mode=false skia_canvaskit_enable_debugger=false skia_canvaskit_enable_paragraph=false skia_canvaskit_enable_webgl=false skia_canvaskit_enable_webgpu=false'
+$GN gen out/Vulkan --args='is_official_build=false cxx="$cxx" cc="$cc" skia_use_vulkan=true skia_use_vma=true skia_enable_gpu=true skia_enable_tools=false skia_use_gl=false skia_use_expat=true skia_enable_fontmgr_empty=false skia_enable_svg=true skia_use_icu=true is_debug=$dbg is_component_build=false is_trivial_abi=false werror=true skia_use_fonthost_mac=false skia_use_angle=false skia_use_dng_sdk=false skia_use_dawn=false skia_use_webgl=false skia_use_webgpu=false skia_use_fontconfig=false skia_use_freetype=true skia_use_libheif=false skia_use_libjpeg_turbo_decode=false skia_use_libjpeg_turbo_encode=false skia_use_no_jpeg_encode=false skia_use_libpng_decode=true skia_use_libpng_encode=true skia_use_no_png_encode=false skia_use_libwebp_decode=false skia_use_libwebp_encode=false skia_use_no_webp_encode=false  skia_use_lua=false skia_use_piex=false skia_use_system_freetype2=false skia_use_system_libpng=false skia_use_system_zlib=false skia_use_wuffs=false skia_use_zlib=false skia_enable_ganesh=true skia_build_for_debugger=false skia_enable_sksl_tracing=false skia_enable_skshaper=false skia_enable_skparagraph=false skia_enable_pdf=false skia_canvaskit_force_tracing=false skia_canvaskit_profile_build=false skia_canvaskit_enable_skp_serialization=false skia_canvaskit_enable_effects_deserialization=false skia_canvaskit_enable_skottie=false skia_canvaskit_include_viewer=false skia_canvaskit_enable_pathops=false skia_canvaskit_enable_rt_shader=false skia_canvaskit_enable_matrix_helper=false skia_canvaskit_enable_canvas_bindings=false skia_canvaskit_enable_font=false skia_canvaskit_enable_embedded_font=false skia_canvaskit_enable_alias_font=false skia_canvaskit_legacy_draw_vertices_blend_mode=false skia_canvaskit_enable_debugger=false skia_canvaskit_enable_paragraph=false skia_canvaskit_enable_webgl=false skia_canvaskit_enable_webgpu=false'
 
 # run build
 $NINJA -C out/Vulkan
--- overlays/skia/ninja.bat
+++ overlays/skia/ninja.bat
@@ -2,12 +2,14 @@
 
 setlocal enabledelayedexpansion
 set sdk=%1
+set cfg=%2
 
 third_party\ninja\ninja -C out\Vulkan
 
-echo Sdk is set to %sdk%
+echo sdk is set to %sdk%
+echo cfg is set to %cfg%
 
-if not exist ".\..\install\%sdk%\include\skia" mkdir ..\..\install\%sdk%\include\skia
-xcopy include ..\..\install\%sdk%\include\skia\ /E /I /Y
-xcopy modules ..\..\install\%sdk%\include\skia\ /E /I /Y
-copy /Y out\Vulkan\skia.lib ..\..\install\%sdk%\lib\
+if not exist ".\..\install\%sdk%-%cfg%\include\skia" mkdir ..\..\install\%sdk%-%cfg%\include\skia
+xcopy include ..\..\install\%sdk%-%cfg%\include\skia\ /E /I /Y
+xcopy modules ..\..\install\%sdk%-%cfg%\include\skia\ /E /I /Y
+copy /Y out\Vulkan\skia.lib ..\..\install\%sdk%-%cfg%\lib\
