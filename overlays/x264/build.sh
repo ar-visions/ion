a6d245827e87cb6166d1492f2c6bf8b5b168ebd6
Kalen Novis White <kalen@krworks.com>
2023-12-12 12:44:55 -0700

    simplify install, by installing

--- overlays/x264/build.sh
+++ overlays/x264/build.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-echo "prefix=$1
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-
-Name: x264
-Description: H.264 (MPEG4 AVC) encoder library
-Version: 0.164.3106
-Libs: -L\${exec_prefix}/lib -lx264 -lpthread -lm -ldl
-Libs.private: 
-Cflags: -I\${prefix}/include" >> "$1/lib/pkgconfig/x264.pc"
-
 ./configure \
 	--enable-static \
     --enable-debug \
     --prefix=$1 \
-    && make -j16
+    && make -j16 && make install
 
 # check if build succeeded
 if [ $? -ne 0 ]; then
